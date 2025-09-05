c80273ab04772fcc419aa7fa498f577743654d3d
iosh <ioshx@foxmail.com>
2025-09-05 17:50:27 +0800

    format and add turbo

--- extension/entrypoints/popup/App.tsx
+++ extension/entrypoints/popup/App.tsx
@@ -1,21 +1,13 @@
-import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
-import './App.css';
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://wxt.dev" target="_blank">
-          <img src={wxtLogo} className="logo" alt="WXT logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <div></div>
       <h1>WXT + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>

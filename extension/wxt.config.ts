b449a7e981d06c820e90e9248f78921d5f933c44
iosh <ioshx@foxmail.com>
2025-09-12 20:13:25 +0800

    use mv3

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  manifestVersion: 3,
   modules: ["@wxt-dev/module-react"],
   srcDir: "src",
   imports: false,

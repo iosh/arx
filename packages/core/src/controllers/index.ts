b281b8b7a60e0b94fe2f6eccf1995d38d9070f5e
iosh <ioshx@foxmail.com>
2025-09-27 10:06:36 +0800

    add exports

---
+++ packages/core/src/controllers/index.ts
@@ -0,0 +1,11 @@
+export * from "./account/AccountController.js";
+export * from "./account/types.js";
+
+export * from "./approval/ApprovalController.js";
+export * from "./approval/types.js";
+
+export * from "./network/NetworkController.js";
+export * from "./network/types.js";
+
+export * from "./permission/PermissionController.js";
+export * from "./permission/types.js";
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,5 @@
+export * from "./controllers/index.js";
+
 export * from "./errors/index.js";
+
 export * from "./messenger/index.js";
